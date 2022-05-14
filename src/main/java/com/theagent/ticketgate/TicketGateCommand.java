d128f80466e2622dcdee99a117635df342c61f69
David (_The_Agent_) <davidolbertz@gmail.com>
2022-05-14 20:11:41 +0200

    Updated command messages

--- src/main/java/com/theagent/ticketgate/TicketGateCommand.java
+++ src/main/java/com/theagent/ticketgate/TicketGateCommand.java
@@ -1,9 +1,7 @@
 package com.theagent.ticketgate;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
 public class TicketGateCommand implements CommandExecutor {
@@ -16,15 +14,15 @@ public class TicketGateCommand implements CommandExecutor {
             Player p = (Player) sender;
             if (args.length == 0) {
                 // if only base command
-                p.sendMessage(ChatColor.GOLD + "Nice! The plugin seems to work");
+                p.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Nice! The plugin seems to work");
                 // TODO better message like information about the plugin and it's usage
             } else if (args.length == 2) {
                 if (args[0].equals("test")) {
-                    p.sendMessage("[TicketGate] Test hat funktioniert! Nachricht: " + args[1]);
+                    p.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Test hat funktioniert! Nachricht: " + args[1]);
                 }
+            } else {
+                p.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Your command seems to be wrong :/ | Try /ticketgate help");
             }
-        } else {
-            // TODO Error Message when using console or command block
         }
         return false;
     }
