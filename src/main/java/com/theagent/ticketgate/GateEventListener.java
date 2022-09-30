6536ab1572244465d1bbf108e5c38a53aed0eeaf
David (_The_Agent_) <davidolbertz@gmail.com>
2022-09-30 21:37:28 +0200

    added several commands and implemented the ability to use different gate configurations

--- src/main/java/com/theagent/ticketgate/GateEventListener.java
+++ src/main/java/com/theagent/ticketgate/GateEventListener.java
@@ -3,21 +3,28 @@ package com.theagent.ticketgate;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.List;
 import java.util.Objects;
 
 public class GateEventListener implements Listener {
 
     private TicketGate main; // so the config file can be used here
+    private FileConfiguration config; // so the config file can be used here
 
     public GateEventListener (TicketGate main) {
         this.main = main;
+        config = main.getConfig();
     }
 
     @EventHandler
@@ -26,21 +33,31 @@ public class GateEventListener implements Listener {
 
         if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
             // if it is a right click on a block:
-            if (Objects.requireNonNull(e.getClickedBlock()).getType().equals(Material.ACACIA_FENCE_GATE)) {
+            Block clickedBlock = e.getClickedBlock();
+            Material clickedBlockType = clickedBlock.getType();
+            ItemStack item = p.getInventory().getItemInMainHand();
+            String itemKey = itemLore(item);
+
+            if (Objects.requireNonNull(clickedBlock.getType().equals(Material.ACACIA_FENCE_GATE))) {
                 // if it is the correct gate type:
                 if (p.isSneaking() && main.getConfig().getBoolean("allow-illegal-bypass")) {
                     // if the player is sneaking
-                    Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "* " + p.getDisplayName() + " jumps over a ticket gate illegally *");
-                    p.sendMessage(ChatColor.RED + "[TicketGate] Feature not implemented yet!"); // TODO implementation of illegal use
+                    illegalMessage(p, clickedBlock);
+                    /*
+                     * Temporary solution: Just open the gate
+                     * Correct way would be the player teleporting behind the gate.
+                     */
                 } else {
-                    if (p.getInventory().getItemInMainHand().getType().equals(Material.PAPER)) {
+                    if (item.getType().equals(Material.PAPER) && itemKey != null && checkTicket(p, item, clickedBlock)) {
                         // if the player has a ticket:
-                        p.sendMessage(ChatColor.GREEN + "Du darfst das Tor jetzt nutzen"); // TODO Message yes or no?
+                        p.sendMessage(ChatColor.GREEN + "Success! (ItemKey:" + itemKey + ")"); // TODO Message yes or no? --> NO, REMOVE IT AFTER TESTING!
                         // TODO different ticket types (defined in item lore, specified in config file)
+                    } else if (getGateType(clickedBlock).equals("default")) {
+                        // allow opening the gate if it is a default gate
                     } else {
                         // if not cancel the event
                         e.setCancelled(true);
-                        p.sendMessage(ChatColor.RED + "Du hast kein Ticket (Papier)!"); // TODO Message yes or no?
+                        p.sendTitle("§4§lWrong ticket!", "§4You need a ticket to enter!", 10, 70, 20); // TODO Test title
                     }
                 }
             }
@@ -55,6 +72,56 @@ public class GateEventListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent e) { // TODO remove when finished
         Player p = e.getPlayer();
         p.sendMessage(ChatColor.RED + "[TicketGate] Warning: This Plugin is in early development so don't expect everything to work flawlessly!");
+    }
+
+    private String getGateType(Block block) {
+        Material floor = block.getLocation().subtract(0, 1, 0).getBlock().getType(); // get block below gate
+        String[] gates = config.getConfigurationSection("gates").getKeys(false).toArray(new String[0]); // get all possible gate configurations
+        String gate = "default";
+
+        // search if the block fits a gate configuration
+        for (String gateConfig : gates) {
+            if (floor.equals(Material.getMaterial(config.getString("gates." + gateConfig + ".block")))) {
+                gate = gateConfig;
+                break;
+            }
+        }
+
+        return gate;
+    }
+
+    private boolean checkTicket(Player player, ItemStack item, Block block) {
+        String key = itemLore(item); // get the key of the ticket
+
+        String gate = getGateType(block); // get the gate type
+
+        // the default gate does not need a ticket
+        if (gate.equals("default")) {
+            return true;
+        }
+
+        // check if the gate configuration fits the gate
+        if (key.equals(config.getString("gates." + gate + ".id"))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String itemLore(ItemStack item) {
+        if (item.hasItemMeta()) {
+            ItemMeta meta = item.getItemMeta();
+            if (meta.hasLore()) {
+                List<String> lore = meta.getLore();
+                if (lore.size() >= 2) {
+                    return lore.get(1);
+                } else return null;
+            } else return null;
+        } else return null;
+    }
+
+    private void illegalMessage(Player player, Block block) {
+        Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "* " + player.getDisplayName() + " opened a ticket gate illegally at " + block.getX() + ", " + block.getY() + ", " + block.getZ() + " *");
     }
 
 }
--- src/main/java/com/theagent/ticketgate/TicketGateCommand.java
+++ src/main/java/com/theagent/ticketgate/TicketGateCommand.java
@@ -1,32 +1,136 @@
 package com.theagent.ticketgate;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.*;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class TicketGateCommand implements CommandExecutor {
 
+    private TicketGate main;
+    private FileConfiguration config;
+
+    public TicketGateCommand(TicketGate main) {
+        this.main = main;
+        config = main.getConfig();
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // check if sender is player and has permission
+        // check if the sender is a player and has permission
         if (sender instanceof Player && sender.hasPermission("ticketgate.use")) {
-            // cast sender as player
-            Player p = (Player) sender;
-            if (args.length == 0) {
-                // if only base command
-                p.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Nice! The plugin seems to work");
-                // TODO better message like information about the plugin and it's usage
-            } else if (args.length == 2) {
-                if (args[0].equals("test")) {
-                    p.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Test hat funktioniert! Nachricht: " + args[1]);
-                }
-            } else {
-                p.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Your command seems to be wrong :/ | Try /ticketgate help");
+            // cast sender to player
+            Player player = (Player) sender;
+
+            switch (args.length) {
+                case 0:
+                    // TODO add message for help or information on the plugin
+                    player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Information page coming soon!");
+                    break;
+                case 2:
+                    if (args[0].equals("setGate")) {
+                        setGate(player, args[1]);
+                    } else if (args[0].equals("remove")) {
+                        removeGate(player, args[1]);
+                    } else if (args[0].equals("ticket")) {
+                        giveTicket(player, args[1]);
+                    }
+                    break;
+                case 3:
+                    if (args[0].equals("add")) {
+                        if (addGate(args)) {
+                            player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Gate added!");
+                        } else {
+                            player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Gate could not be added!");
+                        }
+                    }
+                    break;
+                default:
+                    player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Your command seems to be wrong :/ | Try /ticketgate help");
+                    break;
             }
         } else {
             sender.sendMessage("[TicketGate] Commands can only be used by a player!");
         }
         return false;
+    }
+
+    private boolean addGate(String[] args) {
+        if (!config.contains("gates." + args[1])) {
+            config.set("gates." + args[1] + ".gate", "ACACIA_FENCE_GATE");
+            config.set("gates." + args[1] + ".block", args[2]);
+            config.set("gates." + args[1] + ".id", generateID());
+            main.saveConfig();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void removeGate(Player player, String name) {
+        if (config.contains("gates." + name)) {
+            if (!name.equals("default")) {
+                config.set("gates." + name, null);
+                main.saveConfig();
+                player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Gate removed!");
+            } else {
+                player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + " ] You can't remove the default gate!");
+            }
+        } else {
+            player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Gate could not be removed!");
+        }
+    }
+
+    private void giveTicket(Player player, String name) {
+        if (config.contains("gates." + name)) {
+            if (!name.equals("default")) {
+                List<String> lore = new ArrayList<>(); // the item's lore will be saved in this list
+                lore.add(config.getString("gates." + name + ".lore")); // add the lore to the list
+                lore.add(config.getString("gates." + name + ".id")); // add the id to the list
+
+                ItemStack ticket = new ItemStack(Material.PAPER); // create a new item (paper)
+                ItemMeta ticketMeta = ticket.getItemMeta(); // get the item's meta
+                ticketMeta.setDisplayName(config.getString("gates." + name + ".name")); // set the item's name
+                ticketMeta.setLore(lore); // set the item's lore
+
+                ticket.setItemMeta(ticketMeta); // add updated meta back to the item
+                player.getInventory().addItem(ticket); // give the player the ticket
+
+                player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Ticket given!");
+            } else {
+                player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] You don't need a ticket for the default gate!");
+            }
+        } else {
+            player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Ticket could not be given!");
+        }
+    }
+
+    private String generateID() {
+        Random randomizer = new Random();
+        int id = randomizer.nextInt(999999 - 100000) + 100000;
+        return Integer.toString(id);
+    }
+
+    private void setGate(Player player, String name) {
+        Block gate = player.getTargetBlockExact(3);
+        if (gate != null && gate.getType().name().equals(config.get("gates." + name + ".gate"))) {
+            Block floor = player.getTargetBlockExact(3).getLocation().subtract(0, 1, 0).getBlock();
+            Material newFloor = Material.getMaterial(config.getString("gates." + name + ".block"));
+            floor.setType(newFloor);
+            player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Gate updated successfully!");
+        } else {
+            player.sendMessage("[" + ChatColor.GOLD + "TicketGate" + ChatColor.RESET + "] Gate not found!");
+        }
     }
 
 }
