9155ee370125b2502450c2a84496cb3c9c38e185
David (_The_Agent_) <davidolbertz@gmail.com>
2022-09-30 23:42:25 +0200

    optimized code

--- src/main/java/com/theagent/ticketgate/TicketGateTabCompleter.java
+++ src/main/java/com/theagent/ticketgate/TicketGateTabCompleter.java
@@ -9,6 +9,9 @@ import org.bukkit.util.StringUtil;
 
 import java.util.*;
 
+/**
+ * A class for tab completion of the /ticketgate command
+ */
 public class TicketGateTabCompleter implements TabCompleter {
 
     FileConfiguration config;
@@ -21,23 +24,31 @@ public class TicketGateTabCompleter implements TabCompleter {
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         switch (args.length) {
             case 1:
-                return StringUtil.copyPartialMatches(args[0], Arrays.asList("add", "remove", "setGate", "ticket"), new ArrayList<>());
+                return StringUtil.copyPartialMatches(args[0], Arrays.asList("add", "remove", "setGate", "ticket", "editBlock", "editName", "editLore"), new ArrayList<>());
             case 2:
-                if (args[0].equals("remove") || args[0].equals("setGate") || args[0].equals("ticket")) {
+                if (args[0].equals("remove") || args[0].equals("setGate") || args[0].equals("ticket") || args[0].equals("editBlock") || args[0].equals("editName") || args[0].equals("editLore")) {
                     Set<String> gates = config.getConfigurationSection("gates").getKeys(false);
                     return StringUtil.copyPartialMatches(args[1], gates, new ArrayList<>());
                 } else return new ArrayList<>();
             case 3:
-                if (args[0].equals("add")) {
-                    List<String> materials = new ArrayList();
-                    for (Material material : Material.values()) {
-                        materials.add(material.name());
-                    }
-                    return StringUtil.copyPartialMatches(args[2], materials, new ArrayList<>());
+                if (args[0].equals("add") || args[0].equals("editBlock")) {
+                    return StringUtil.copyPartialMatches(args[2], getMaterials(), new ArrayList<>());
                 } else return new ArrayList<>();
             default:
                 return new ArrayList<>();
         }
+    }
+
+    /**
+     * Gets a list of all materials
+     * @return a list of all materials
+     */
+    private List<String> getMaterials() {
+        List<String> materials = new ArrayList();
+        for (Material material : Material.values()) {
+            materials.add(material.name());
+        }
+        return materials;
     }
 
 }
