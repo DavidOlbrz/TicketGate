948201841d881f257135e5194a60716c162fa102
DavidOlbrz <davidolbertz@gmail.com>
2025-04-02 22:00:57 +0200

    renamed variable

--- src/main/java/com/theagent/ticketgate/PlayerMessenger.java
+++ src/main/java/com/theagent/ticketgate/PlayerMessenger.java
@@ -7,24 +7,26 @@ import org.bukkit.entity.Player;
  */
 public class PlayerMessenger {
 
-    static final String prefix = "[§6TicketGate§r] "; // default prefix
+    private static final String PREFIX = "[§6TicketGate§r] "; // default prefix
 
     /**
      * Sends a message to the player
-     * @param player the player to send the message to
+     *
+     * @param player  the player to send the message to
      * @param message the message to send
      */
     public static void sendMessage(Player player, String message) {
-        player.sendMessage(prefix + message);
+        player.sendMessage(PREFIX + message);
     }
 
     /**
      * Sends an error message to the player
-     * @param player the player to send the message to
+     *
+     * @param player  the player to send the message to
      * @param message the error message to send
      */
     public static void sendError(Player player, String message) {
-        player.sendMessage(prefix + "§c" + message);
+        player.sendMessage(PREFIX + "§c" + message);
     }
 
 }
